bf2f76455e3e6d897d354aa3e6282e198cb2f238
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-06-02 23:51:53 +0530

    example1.dart

--- operators/bin/example1.dart
+++ operators/bin/example1.dart
@@ -2,7 +2,7 @@ void main(List<String> args) {
   const age1 = 64;
   const age2 = 30;
   print(age1 + age2);
-  print(age1 + age2 + 30);
+  print(age1 + age2 + 30); 
   print(age1 - age2);
   print(age1 * age2);
   const double divided = age1 / age2;
